96fdc87772cedca433bddbde957ffb70a2241f03
Adnan Imran <69186678+AdnanSalazzar@users.noreply.github.com>
2023-05-16 21:57:37 +0600

    ding the map

--- starter/script.js
+++ starter/script.js
@@ -10,3 +10,31 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+///                                    parameter ==> success func , failure func
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    console.log(position);
+    const { longitude } = position.coords;
+    const { latitude } = position.coords;
+    console.log(latitude, longitude);
+
+    console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    const coords = [latitude , longitude]
+                                        ///ZOOM initial 
+    const map = L.map('map').setView(coords, 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker(coords)
+      .addTo(map)
+      .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+      .openPopup();
+  },
+  function () {
+    alert('Could not get the position');
+  }
+);
